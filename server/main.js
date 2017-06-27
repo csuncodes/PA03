ce5a24bc65e042ae09ec7eddca964ea20399fa4c
Claire Sun <sun.claire32@gmail.com>
2017-06-27 00:04:33 -0400

    PA03 upload

---
+++ server/main.js
@@ -0,0 +1,8 @@
+import { Meteor } from 'meteor/meteor';
+
+Meteor.startup(() => {
+  // code to run on server at startup
+  People.remove({});  // clear the database
+  People.insert({name:'Tim',food: "Japanese", date: "07/07/2017", num: "5"});
+  People.insert({name:'Caitlin',food:"Chinese", date: "09/12/2017", num: "3"});
+});
